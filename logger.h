87af1379c96fc47a3047069bfb874523aed5657c
JonghHyeon <jonghp1128@naver.com>
2025-12-09 13:50:24 +0900

    [feat] 로그 구현

--- logger.h
+++ logger.h
@@ -12,19 +12,41 @@ private:
     static std::mutex write_mtx;
     std::ofstream logFile;
 
+    Logger() {
+        Logger::logFile.open("Test/output2.txt", std::ios::trunc);
+        if (Logger::logFile.is_open()) {
+            Logger::logFile << "[Init] Logger started." << std::endl;
+        } else {
+            throw std::runtime_error("Failed to open log file");
+        }
+    }
 
 public:
+    ~Logger() {
+        if (Logger::logFile.is_open()) {
+            Logger::logFile << "[Shutdown] Logger closed." << std::endl;
+            Logger::logFile.close();
+        }
+    }
+
     static Logger* getInstance(const std::string& filename = "Test/output2.txt") {
-        return nullptr;
+        std::lock_guard<std::mutex> lock(init_mtx);
+        if (!Logger::instance) {
+            Logger::instance.reset(new Logger());
+        }
+        return instance.get();
     }
 
     void log(const std::string& message) {
         /* TODO */
+        std::lock_guard<std::mutex> lock(write_mtx);
+        Logger::logFile << message << std::endl;
     }
+
+    
 
 };
 
 std::unique_ptr<Logger> Logger::instance;
 std::mutex Logger::init_mtx;
 std::mutex Logger::write_mtx;
-
